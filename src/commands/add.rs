eef14bf1b6d4caeec320581aacbcb578861fa29b
Lalit Yadav <lalityadav.x17@gmail.com>
2025-07-16 17:52:38 -0500

    lit add command completed

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -1,14 +1,19 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Result};
+use flate2::{Compression, write::ZlibEncoder};
+use sha1::{Digest, Sha1};
+use std::collections::HashMap;
+use std::fs;
+use std::fs::{File, read};
+use std::io::{BufRead, BufReader, Result, Write};
 use std::path::Path;
 use walkdir::WalkDir;
 
 pub fn add(dir_path: &String) -> Result<()> {
-    let mut files: Vec<String> = Vec::<String>::new();
-    files = get_all_files_recursively(&dir_path);
+    let files = get_all_files_recursively(&dir_path);
+
+    let mut file_map: HashMap<String, String> = read_index();
 
     for file in &files {
-        println!("{}", file);
+        encrypt_and_store(file, &mut file_map)?;
     }
 
     Ok(())
@@ -51,4 +56,69 @@ pub fn get_all_files_recursively(dir_path: &String) -> Vec<String> {
         })
         .filter(|path| !is_ignored(&path, &ignore_files))
         .collect()
+}
+
+fn read_index() -> HashMap<String, String> {
+    let mut file_hash: HashMap<String, String> = HashMap::new();
+    if let Ok(file) = File::open(".lit/index") {
+        let reader = BufReader::new(file);
+        for line in reader.lines().flatten() {
+            let mut parts = line.split_whitespace();
+            if let (Some(hash), Some(file_path)) = (parts.next(), parts.next()) {
+                file_hash.insert(file_path.to_string(), hash.to_string());
+            }
+        }
+    }
+
+    return file_hash;
+}
+
+fn create_blob_object(file_path: &str) -> Result<(Vec<u8>, String)> {
+    let content = read(file_path)?;
+    let size = content.len();
+
+    let header = format!("blob {}\0", size);
+
+    let mut blob_data = Vec::new();
+    blob_data.extend_from_slice(header.as_bytes());
+    blob_data.extend_from_slice(&content);
+
+    let mut hasher = Sha1::new();
+    hasher.update(&blob_data);
+    let hash = hasher.finalize();
+    let hash_hex = format!("{:x}", hash);
+
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(&blob_data)?;
+    let compressed_blob = encoder.finish()?;
+
+    Ok((compressed_blob, hash_hex))
+}
+
+fn store_hash_to_index(file_map: &HashMap<String, String>) -> Result<()> {
+    let mut file = File::create(".lit/index")?;
+    for (file_path, hash) in file_map {
+        writeln!(file, "{} {}", hash, file_path)?;
+    }
+
+    Ok(())
+}
+
+pub fn encrypt_and_store(file_path: &String, file_map: &mut HashMap<String, String>) -> Result<()> {
+    let (compressed_blob, hash) = create_blob_object(file_path)?;
+
+    if file_map.get(file_path) != Some(&hash) {
+        file_map.insert(file_path.clone(), hash.clone());
+        store_hash_to_index(&file_map)?;
+    }
+
+    let (dir, file) = hash.split_at(2);
+    let object_dir = format!(".lit/objects/{}", dir);
+    let object_path = format!("{}/{}", object_dir, file);
+
+    fs::create_dir_all(&object_dir)?;
+
+    let mut cur_file = fs::File::create(&object_path)?;
+    cur_file.write_all(&compressed_blob)?;
+    Ok(())
 }
--- src/commands/init.rs
+++ src/commands/init.rs
@@ -3,8 +3,8 @@ use std::io::Result;
 use std::io::Write;
 
 pub fn init() -> Result<()> {
-    fs::create_dir_all("./lit/objects")?;
-    fs::create_dir_all("./lit/refs/heads")?;
+    fs::create_dir_all("./.lit/objects")?;
+    fs::create_dir_all("./.lit/refs/heads")?;
     fs::write(".lit/HEAD", "ref: refs/heads/master\n")?;
     fs::File::create(".lit/refs/heads/main")?.write_all(b"")?;
     fs::File::create(".lit/index")?;
